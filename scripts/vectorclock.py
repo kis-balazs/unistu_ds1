5366ad9a81781765519f6820a515f32a33fffbda
Nils Imhoff <github@d4d1.de>
2022-01-20 21:04:46 +0100

    add vectorclock

---
+++ scripts/vectorclock.py
@@ -0,0 +1,16 @@
+class VectorClock:
+
+    def __init__(self, id):
+        if id == None:
+            self.vcDictionary = {}
+        else:
+            self.vcDictionary = {(id): 0}
+
+    def increaseClock(self, id):
+        self.vcDictionary[(id)] += 1
+
+    def addParticipantToClock(self, id):
+        self.vcDictionary[id] = 0
+
+    def printClock(self):
+        print("\n[VECTORCLOCK]: ", str(self.vcDictionary))
