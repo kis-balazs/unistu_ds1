e3ea155e5d83eb11510baa1144385e99867dc631
Marcel Marquardt <marcel@mardom.dev>
2022-02-09 14:18:24 +0100

    Remove unused files

--- scripts/client.py
+++
@@ -1,7 +0,0 @@
-import logging
-from discovery_server_thread import find_primary
-
-
-if __name__ == '__main__':
-	logging.basicConfig(format='[%(asctime)s] [%(levelname)-05s] %(message)s', level=logging.DEBUG)
-	find_primary()
